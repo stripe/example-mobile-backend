8e73f7206d715d17f26aa5101be6fcced17e1f39
Ben Guo <bg@stripe.com>
2016-04-28 17:15:14 -0400

    update example with customer endpoints

--- Gemfile
+++ Gemfile
@@ -3,3 +3,4 @@ ruby "2.1.2"
 gem 'sinatra', '1.4.5'
 gem 'stripe', '1.20.0'
 gem 'dotenv', '1.0.2'
+gem 'json', '1.8.2'
--- web.rb
+++ web.rb
@@ -1,6 +1,7 @@
 require 'sinatra'
 require 'stripe'
 require 'dotenv'
+require 'json'
 
 Dotenv.load
 
@@ -14,22 +15,89 @@ end
 post '/charge' do
 
   # Get the credit card details submitted by the form
-  token = params[:stripeToken]
+  token = params[:stripe_token]
+  customer = params[:customer]
 
   # Create the charge on Stripe's servers - this will charge the user's card
   begin
     charge = Stripe::Charge.create(
       :amount => params[:amount], # this number should be in cents
       :currency => "usd",
-      :card => token,
+      :customer => customer,
+      :source => token,
       :description => "Example Charge"
     )
-  rescue Stripe::CardError => e
+  rescue Stripe::StripeError => e
     status 402
     return "Error creating charge."
   end
 
   status 200
-  return "Order successfully created"
+  return "Charge successfully created"
+
+end
+
+get '/cards' do
+
+  customer = params[:customer]
+
+  begin
+    # Retrieves the customer's cards
+    customer = Stripe::Customer.retrieve(customer)
+  rescue Stripe::StripeError => e
+    status 402
+    return "Error retrieving cards."
+  end
+
+  status 200
+  content_type :json
+  cards = customer.sources.all(:object => "card")
+  selected_card = cards.find {|c| c.id == customer.default_source}
+  return { :cards => cards.data, selected_card: selected_card }.to_json
+
+end
+
+post '/add_token' do
+
+  token = params[:stripe_token]
+  customer = params[:customer]
+
+  # Adds the token to the customer's sources
+  begin
+    customer = Stripe::Customer.retrieve(customer)
+    customer.sources.create({:source => token})
+  rescue Stripe::StripeError => e
+    status 402
+    return "Error adding token to customer."
+  end
+
+  status 200
+  content_type :json
+  cards = customer.sources.all(:object => "card")
+  selected_card = cards.find {|c| c.id == customer.default_source}
+  return { :cards => cards.data, selected_card: selected_card }.to_json
+
+end
+
+post '/select_source' do
+
+  source = params[:source]
+  customer = params[:customer]
+
+  # Sets the customer's default source
+  begin
+    customer = Stripe::Customer.retrieve(customer)
+    customer.default_source = source
+    customer.save
+  rescue Stripe::StripeError => e
+    status 402
+    return "Error selecting default source."
+  end
+
+  status 200
+  content_type :json
+  cards = customer.sources.all(:object => "card")
+  selected_card = cards.find {|c| c.id == customer.default_source}
+  return { :cards => cards.data, selected_card: selected_card }.to_json
 
 end
