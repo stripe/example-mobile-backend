f6d2429bb7a158e2de4150fb38a11a3f38dd3485
Kathy Sun <ksun@stripe.com>
2017-10-09 22:15:20 -0700

    logs

--- web.rb
+++ web.rb
@@ -36,7 +36,8 @@ post '/charge' do
   # Get the credit card details submitted by the form
   source = params[:source]
   customer = params[:customer_id] || @customer.id
-  
+  puts '[Params]'
+  p params
   # Create the charge on Stripe's servers - this will charge the user's card
   begin
     charge = Stripe::Charge.create(
